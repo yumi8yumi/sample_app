ecf21bf6376940d2b32926505ac2c547c30b4d05
yumi8yumi <yumikokaho@gmail.com>
2022-04-02 23:47:09 +0000

    [修正],[end]3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists'
-  get 'lists/index'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get 'top' => "homes#top"
