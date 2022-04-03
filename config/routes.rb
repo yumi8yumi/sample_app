41653088976026e3c0d645c25a2ff5b7459f930f
yumi8yumi <yumikokaho@gmail.com>
2022-04-03 05:37:10 +0000

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit' ,as:'edit_list'
   get 'top' => "homes#top"
   patch  'lists/:id' =>'lists#update' ,as:'update_list'
+  delete 'lists/:id' => 'lists#destroy', as:'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
