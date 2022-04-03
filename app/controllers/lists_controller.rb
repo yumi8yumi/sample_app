de30fe235d54e44d67c3abfade8e97d15a34ae9d
yumi8yumi <yumikokaho@gmail.com>
2022-04-03 01:44:16 +0000

    [end]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def new
-    @list =List.new
+    @list = List.new
   end
 
  def create
@@ -18,6 +18,23 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find (params[:id])
+  end
+  
+  def update
+    list = List.find (params[:id])
+    list.update (list_params)
+    redirect_to list_path (list.id)
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
   
   private
